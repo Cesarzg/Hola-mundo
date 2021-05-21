3ebf0796544762fae83975234c3b7ff5f61693b8
Cesarzg <76677457@certus.edu.pe>
2021-05-21 17:33:14 -0500

    1

---
+++ 02.py
@@ -0,0 +1,3 @@
+nombre=input("El nombre es :")
+numero=input("Ingrese un numero :")
+print(nombre*int(numero))
---
+++ Actividad.py
@@ -0,0 +1,12 @@
+Horas=float(input("horas trabajadas :"))
+Costo=float(input("costo por hora :"))
+Paga=(Horas*Costo)
+print("la paga es",Paga)
+
+
+N=int(input("El valor de N es :"))
+suma=(N(N+1))/2
+print("La suma es :",suma)
+
+
+nombre
